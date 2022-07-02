01b3c603596191e0f0bf555cef674edbaf9ba514
Tim Miner <poeminminer1@gmail.com>
2022-07-02 16:18:16 -0500

    added render function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -16,9 +16,40 @@ function getTasks() {
     })
     .then(function(response) {
         console.log(response);
-        renderBooks(response);
+        render(response);
     })
     .catch(function(error){
         console.log('error in GET', error);
     });
+}
+
+// Shows data from SQL to the DOM. Also appends a button for isComplete
+function render(todoList) {
+    $('.task-container').empty();
+
+    for(let i = 0; i< todoList.length; i++) {
+        let currentTask = todoList[i];
+
+        $('.task-container').append(`
+        <tr>
+            <td>${currentTask.id}</td>
+            <td>${currentTask.task}</td>
+            <td>${currentTask.importance}</td>
+            <td>${currentTask.isComplete}</td>
+            <td>
+                <button
+                class="button completeButton"
+                data-id=${currentTask.id}
+                data-isComplete= ${currentTask.isComplete}>Complete
+                </button>
+            </td>
+            <td>
+                <button class="button deleteButton"
+                data-id= ${currentTask.id}
+                >Delete
+                </button>
+            </td>
+        </tr>
+        `)
+    }
 }
--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const pool = require('../modules/pool');
 
 router.get('/', (req, res) => {
-    let queryText = 'SELECT * FROM "to-do-list"ORDER BY "importance";';
+    let queryText = 'SELECT * FROM "to-do-list"ORDER BY "importance" ASC;';
     pool.query(queryText).then(result => {
         // Sends back the results in an object
         res.send(result.rows);
