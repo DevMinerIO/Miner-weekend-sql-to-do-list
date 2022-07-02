efe0a2f453942fb97a8310e40a3c233b5a88f745
Tim Miner <poeminminer1@gmail.com>
2022-07-02 15:07:47 -0500

    boiler plate complete. server working on port 5000 and jquery functioning

---
+++ server/routes/todo.router.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool');
+
+
+
+
+
+
+
+
+
+
+
+
+
+// router export at the bottom
+module.exports = router;
