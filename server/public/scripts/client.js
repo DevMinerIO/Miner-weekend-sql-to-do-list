268f50fd5072608c6d2816168f19e5f17bc9301f
Tim Miner <poeminminer1@gmail.com>
2022-07-02 15:28:32 -0500

    added get request function to the client

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
     console.log('Jquery and js working');
+    clickHandlers();
 })
+
+function clickHandlers() {
+
+}
+
+function getTasks() {
+    $.ajax({
+        method: 'GET',
+        url: '/todo'
+    })
+    .then(function(response) {
+        console.log(response);
+        renderBooks(response);
+    })
+    .catch(function(error){
+        console.log('error in GET', error);
+    });
+}
